f16fe9317d2a7e1ab3ec3fecc6c1019193b54301
Collins ifunanya <collinsogbuzuru@gmail.com>
2018-10-03 22:30:27 +0100

    Entry file

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express()
+
+
+
+mongoose.connect("mongodb://collins:collins123@ds121753.mlab.com:21753/busterminal").then((res)=>{
+  console.log('connected to database')
+}).catch((error)=>{
+    console.log(error)
+    process.exit(1)
+})
+
+
+app.use('/ulo',graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+
+
+
+
+app.listen(4000,()=>{
+    console.log('we are live on 4000')
+})
